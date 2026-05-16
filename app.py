b8d129732383e70151aa48dd4ed9b6bc996f0259
TitoWin94 <88687143+TitoWin94@users.noreply.github.com>
2026-05-16 09:56:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ st.title("Proyecto Final Casa Grande")
 st.sidebar.title("Parametros")
 st.sidebar.image("Python_logo.png")
 
+uploaded_files = st.file_uploader(
+
+    "Upload data", accept_multiple_files=True, type="csv"
+
+)
+
+for uploaded_file in uploaded_files:
+
+    df = pd.read_csv(uploaded_file)
+
+    st.write(df)
